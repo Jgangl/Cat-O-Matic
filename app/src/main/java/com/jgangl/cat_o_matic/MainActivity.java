c5a8f5750e64716c2a10222008da644c1f6e77db
Jgangl <joshua.gangl@gmail.com>
2020-09-07 01:56:57 -0400

    Redoing

--- app/src/main/java/com/jgangl/cat_o_matic/MainActivity.java
+++ app/src/main/java/com/jgangl/cat_o_matic/MainActivity.java
@@ -8,6 +8,7 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -15,56 +16,36 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity{
 
     Button btnTimePicker;
-    EditText txtTime;
+    Button manualMealButton;
+    Switch enableAllMeals;
+
+    EditText mealOneTime, mealTwoTime, mealThreeTime, mealFourTime, mealFiveTime;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtTime=(EditText)findViewById(R.id.in_time);
+        mealOneTime=(EditText)findViewById(R.id.in_time);
 
-        txtTime.setInputType(InputType.TYPE_NULL);
-        txtTime.setOnClickListener(new View.OnClickListener() {
+        mealOneTime.setInputType(InputType.TYPE_NULL);
+        mealOneTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTimeDialog();
             }
         });
-        txtTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        mealOneTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    showTimeDialog();
-                }
+            if (hasFocus) {
+                showTimeDialog();
+            }
             }
         });
-
     }
-/*
-    @Override
-    public void onClick(View v) {
-        if (v == txtTime) {
-            // Get Current Time
-            final Calendar c = Calendar.getInstance();
-            int mHour = c.get(Calendar.HOUR_OF_DAY);
-            int mMinute = c.get(Calendar.MINUTE);
 
-            // Launch Time Picker Dialog
-            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                    new TimePickerDialog.OnTimeSetListener() {
-
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay,
-                                              int minute) {
-
-                            txtTime.setText(hourOfDay + ":" + minute);
-                        }
-                    }, mHour, mMinute, false);
-            timePickerDialog.show();
-        }
-    }
-*/
     private void showTimeDialog(){
         // Get Current Time
         final Calendar c = Calendar.getInstance();
@@ -79,7 +60,7 @@ public class MainActivity extends AppCompatActivity{
                     public void onTimeSet(TimePicker view, int hourOfDay,
                                           int minute) {
 
-                        txtTime.setText(hourOfDay + ":" + minute);
+                        mealOneTime.setText(hourOfDay + ":" + minute);
                     }
                 }, mHour, mMinute, false);
         timePickerDialog.show();
